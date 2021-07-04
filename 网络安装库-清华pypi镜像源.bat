cc5dda2964f30c93fb48c12db1230cddb663c116
3DOnePro <68763066+3DOnePro@users.noreply.github.com>
2021-07-04 16:14:00 +0800

    Add files via upload

---
+++ 网络安装库-清华pypi镜像源.bat
@@ -0,0 +1,12 @@
+@ECHO ON
+pause
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install --upgrade pip 
+pip install colorama 
+pip install lxml  
+pip install requests
+pip install cryptography
+pip install requests
+pip instal datetime
+pip install playsound
+pip instal Thread
